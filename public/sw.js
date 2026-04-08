ad98f2e04e184f5ef8961edfe8322009830ae420
Thiago <thiago@sygmasistemas.com.br>
2026-04-08 14:20:21 -0300

    Test deploy automático

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="obras-v1";
+const CACHE="obras-v2";
 const ASSETS=["/","/index.html","/manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
